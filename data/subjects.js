3a136b83503ed45db0e7d5ba23c867737e8d5a26
techiebookwormart <techiebookworm.art@gmail.com>
2026-09-05 17:03:01 -0500

    Update subjects.js

--- data/subjects.js
+++ data/subjects.js
@@ -1,1 +1,14 @@
-const subjects = [];
+const subjects = [
+  {
+    title: "Subject 1",
+    sourceUrl: "https://www.pinterest.com/"
+  },
+  {
+    title: "Subject 2",
+    sourceUrl: "https://www.pinterest.com/"
+  },
+  {
+    title: "Subject 3",
+    sourceUrl: "https://www.pinterest.com/"
+  }
+];
