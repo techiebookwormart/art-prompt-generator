89c4018773a1243fedc5904061cb8f1b7e89b5d7
techiebookwormart <techiebookworm.art@gmail.com>
2026-09-05 17:21:16 -0500

    Update compositions.js

--- data/compositions.js
+++ data/compositions.js
@@ -2,13 +2,16 @@ const compositions = [
   {
     title: "Bust",
     sourceUrl: "https://www.pinterest.com/"
+    sourceLabel: "View source on Pinterest"
   },
   {
     title: "Half Body",
     sourceUrl: "https://www.pinterest.com/"
+    sourceLabel: "View source on Pinterest"
   },
   {
     title: "Full Body",
     sourceUrl: "https://www.pinterest.com/"
+    sourceLabel: "View source on Pinterest"
   }
 ];
