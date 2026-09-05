4a599e0870b8e75b14b561970bb407e20d325c55
techiebookwormart <techiebookworm.art@gmail.com>
2026-09-05 17:20:01 -0500

    Update subjects.js

--- data/subjects.js
+++ data/subjects.js
@@ -1,14 +1,17 @@
 const subjects = [
-  {
-    title: "Subject 1",
-    sourceUrl: "https://www.pinterest.com/"
-  },
-  {
-    title: "Subject 2",
-    sourceUrl: "https://www.pinterest.com/"
-  },
-  {
-    title: "Subject 3",
-    sourceUrl: "https://www.pinterest.com/"
-  }
+    {
+        title: "Subject 1",
+        sourceUrl: "https://www.pinterest.com/",
+        sourceLabel: "View source on Pinterest"
+    },
+    {
+        title: "Subject 2",
+        sourceUrl: "https://www.pinterest.com/",
+        sourceLabel: "View source on Pinterest"
+    },
+    {
+        title: "Subject 3",
+        sourceUrl: "https://www.pinterest.com/",
+        sourceLabel: "View source on Pinterest"
+    }
 ];
