77d1bba161b9797276048fe1051b6928203a9ee3
techiebookwormart <techiebookworm.art@gmail.com>
2026-09-05 20:26:09 -0500

    Update palettes.js

--- data/palettes.js
+++ data/palettes.js
@@ -1,17 +1,17 @@
 const palettes = [
   {
     title: "Palette 1",
-    sourceUrl: "https://www.pinterest.com/"
+    sourceUrl: "https://www.pinterest.com/",
     sourceLabel: "View source on Pinterest"
   },
   {
     title: "Palette 2",
-    sourceUrl: "https://www.pinterest.com/"
+    sourceUrl: "https://www.pinterest.com/",
     sourceLabel: "View source on Pinterest"
   },
   {
     title: "Palette 3",
-    sourceUrl: "https://www.pinterest.com/"
+    sourceUrl: "https://www.pinterest.com/",
     sourceLabel: "View source on Pinterest"
   }
 ];
